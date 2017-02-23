2aef2f0ede95f888acba1ea2c2f55b22d9597f42
wangyunyan <wyy@zwsoft.com>
2017-02-23 10:50:04 +0800

    sdk samples .props

---
+++ sdk/ZRX2017/utils/amodeler/inc/zamodeler.h
@@ -0,0 +1,25 @@
+#ifndef AECMODELER_INC_H
+#define AECMODELER_INC_H
+
+#define AModeler AECModeler
+
+#include "body.h"
+#include "edge.h"
+//#include "surface.h"
+#include "vertdata.h"
+#include "line3d.h"
+#include "transf3d.h"
+#include "callback.h"
+
+/*
+#include "vertsurf.h"
+#include "vertdata.h"
+#include "dxfinpln.h"
+#include "plane.h"
+#include "deviat.h"
+#include "util.h"
+#include "check.h"
+#include "callback.h"
+*/
+
+#endif
